13417e63b8464e22bfbc9e04a52cee1f5209c0a0
Robin Espicier <roespici@c4r2p6.42nice.fr>
2024-08-27 10:52:46 +0200

    Architecture

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,14 @@
 /*   By: roespici <roespici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 09:45:36 by roespici          #+#    #+#             */
-/*   Updated: 2024/08/27 09:45:37 by roespici         ###   ########.fr       */
+/*   Updated: 2024/08/27 10:48:48 by roespici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft/include/libft.h"
+# include "structs.h"
+
+#endif
