f0346384c41ec163ae6684a0fbf568f1186766c5
ggoy <gaet.jam@gmail.com>
2024-09-16 15:54:16 +0200

    mouarf2

--- srcs/pipex/execution.c
+++ srcs/pipex/execution.c
@@ -48,7 +48,7 @@ void	execute_pipes(t_pipex *pipex)
 static void	execute_child(t_pipex *pipex, int i)
 {
 	open_files(pipex);
-	open_files(pipex);
+	here_signals();
 	if (i == 0)
 	{
 		if (pipex->nb_pipes > 0)
